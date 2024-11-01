03230d0291aaa5220e55bb9101589b2c1b937983
Proxtx <39201896+Proxtx@users.noreply.github.com>
2024-11-01 23:43:06 +0100

    add work manager as a dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,4 +76,6 @@ dependencies {
 
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
+
+    implementation("androidx.work:work-runtime-ktx:2.8.1")
 }
