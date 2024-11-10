c73606ca2acc5b5b809707e7179cb34f54ad2399
Proxtx <39201896+Proxtx@users.noreply.github.com>
2024-11-10 23:59:11 +0100

    test the accessiblity service with a vibrate

--- app/src/main/java/com/proxtx/clip/services/TriggerAccessibilityService.kt
+++ app/src/main/java/com/proxtx/clip/services/TriggerAccessibilityService.kt
@@ -1,13 +1,26 @@
 package com.proxtx.clip.services
 
 import android.accessibilityservice.AccessibilityService
+import android.content.Context
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.os.VibratorManager
 import android.view.KeyEvent
 import android.view.accessibility.AccessibilityEvent
 
 class TriggerAccessibilityService : AccessibilityService() {
+    private lateinit var vibrator: Vibrator
+
     override fun onServiceConnected() {
         super.onServiceConnected()
 
+        vibrator = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val vibratorManager = getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+            vibratorManager.defaultVibrator
+        } else {
+            getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        }
     }
 
     override fun onAccessibilityEvent(p0: AccessibilityEvent?) {
@@ -22,7 +35,7 @@ class TriggerAccessibilityService : AccessibilityService() {
         if(event.action == KeyEvent.ACTION_DOWN) {
             when (event.keyCode) {
                 KeyEvent.KEYCODE_VOLUME_UP -> {
-
+                    vibratePhone()
                 }
                 KeyEvent.KEYCODE_VOLUME_DOWN -> {
 
@@ -31,5 +44,10 @@ class TriggerAccessibilityService : AccessibilityService() {
         }
 
         return super.onKeyEvent(event)
+    }
+
+    private fun vibratePhone() {
+        // Use VibrationEffect for API 26+
+        vibrator.vibrate(VibrationEffect.createOneShot(200, VibrationEffect.DEFAULT_AMPLITUDE))
     }
 }
